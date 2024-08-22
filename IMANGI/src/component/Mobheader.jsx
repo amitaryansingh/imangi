b75be3971c68caf34f92e501660462cdcc67f365
maheshwar04 <maheshwarnag04@gmail.com>
2024-08-23 01:09:25 +0530

    nothing

--- IMANGI/src/component/Mobheader.jsx
+++ IMANGI/src/component/Mobheader.jsx
@@ -6,24 +6,31 @@ import { BiSolidOffer } from "react-icons/bi";
 import { CgProfile } from "react-icons/cg";
 
 const Mobheader = () => {
-  return(<>
-  <div className={style2.navigation}>
-    <div className={style2.navitems}>
-        <li><a href="#">
-            <FaHome className={style1.icon} />
-        Home</a>
-        </li>
-        <li><a href="#">
-        <LuCalendarClock className={style1.icon} />
-        ShowTiming
-        </a>
-        </li>
-        <li><a href="#">
-        <CgProfile className={style1.icon} />
-              Profile</a>
-        </li>
-    </div>
-  </div>
-  </>);
-}
-  export default Mobheader;
+  return (
+    <>
+      <div className={style2.navigation}>
+        <div className={style2.navitems}>
+          <li className={style2.li}>
+            <a href="#">
+              <FaHome className={style1.icon} />
+              Home
+            </a>
+          </li>
+          <li className={style2.li}>
+            <a href="#">
+              <LuCalendarClock className={style1.icon} />
+              ShowTiming
+            </a>
+          </li>
+          <li className={style2.li}>
+            <a href="#">
+              <CgProfile className={style1.icon} />
+              Profile
+            </a>
+          </li>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Mobheader;
