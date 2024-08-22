0f9e7907ffd5d722637099bb0aa2122793929b2c
Anu Raj <aryanamit122@gmail.com>
2024-08-23 01:21:58 +0530

    new

---
+++ IMANGI/src/component/Herovideo.jsx
@@ -0,0 +1,19 @@
+import style1 from "./Header.module.css";
+import style2 from "./Mobheader.module.css";
+import style3 from "./Herovideo.module.css";
+import { FaHome, FaSearch } from "react-icons/fa";
+import { LuCalendarClock } from "react-icons/lu";
+import { BiSolidOffer } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+
+const Herovideo = () => {
+    return (
+      <>
+      <div className="heroview">
+        
+      </div>
+      </>
+    )
+}
+
+export default Herovideo;
