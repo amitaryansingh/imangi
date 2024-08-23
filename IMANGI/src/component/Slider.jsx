a2a3bd3136b368019153ab6b1d1ebadb685c2130
Anu Raj <aryanamit122@gmail.com>
2024-08-23 17:36:17 +0530

    aryanslider

--- IMANGI/src/component/Slider.jsx
+++ IMANGI/src/component/Slider.jsx
@@ -3,16 +3,28 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import style from "./Slider.module.css";
+import { FaAngleLeft, FaAngleRight } from "react-icons/fa";
 
 const ImageSlider = () => {
+  const sliderRef = React.useRef(null);
+
   const settings = {
     dots: true,
     infinite: true,
     speed: 1000,
-    slidesToShow: 5,
-    slidesToScroll: 1,
+    slidesToShow: 3,
+    slidesToScroll:1,
     autoplay: true,
     autoplaySpeed: 3000,
+    arrows: false,
+  };
+
+  const goToNext = () => {
+    sliderRef.current.slickNext();
+  };
+
+  const goToPrev = () => {
+    sliderRef.current.slickPrev();
   };
 
   const images = [
@@ -29,14 +41,16 @@ const ImageSlider = () => {
   ];
 
   return (
-    <div className={style.slidercontainer}>
-      <Slider {...settings}>
-        {images.map((image, index) => (
-          <div key={index} className={style.slickslide}>
-            <img src={image} alt={`Slide ${index + 1}`} />
-          </div>
-        ))}
-      </Slider>
+    <div className={style.sliderWrapper}>
+      <div className={style.slidercontainer}>
+        <Slider ref={sliderRef} {...settings}>
+          {images.map((image, index) => (
+            <div key={index} className={style.slickslide}>
+              <img src={image} alt={`Slide ${index + 1}`} />
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
