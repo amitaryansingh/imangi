9fac244b5b558ebeb3c8bf738e6003e962b1bc81
Anu Raj <aryanamit122@gmail.com>
2024-08-25 15:27:07 +0530

    last

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,44 +1,50 @@
-import React from "react";
-import style from "./Footer.module.css";
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from "react-icons/fa"; // Importing social media icons
+import React from 'react';
+import styles from './Footer.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faTwitter, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
   return (
-    <footer className={style.footer}>
-      <div className={style.footerTop}>
-        <div className={style.logoSection}>
-          <img src="/logo2.png" alt="Imangi-logo" className={style.logo} />
-          <p>
-          Iter College, Jagmohan Nagar Bhubaneswar &51030, Odisha
-            Iter College, Bhubaneswar
-          </p>
+    <footer className={styles.footer}>
+      <div className={styles.footerContent}>
+        <div className={styles.brandTag}>
+          <img src="/logo2.png" alt="Imangi" className={styles.logo} />
         </div>
-        <div className={style.linkSections}>
-          {/* Updated About Us link to scroll to History of Imangi */}
-          <a href="#history-of-imangi">About Us</a>
-          <a href="/terms">Terms of Service</a>
-          <a href="/locations">Locations</a>
-          <a href="/faq">FAQ</a>
-          <a href="/privacy-policy">Privacy Policy</a>
-          <a href="/feedback">Feedback</a>
+        <div className={styles.rightSection}>
+          <div className={styles.address}>
+            <p>
+              1234 Imangi St, Suite 567<br />
+              City, State, 12345
+            </p>
+          </div>
         </div>
       </div>
-      <div className={style.footerBottom}>
-        <div className={style.socialMedia}>
-          <a href="https://facebook.com/YourPage" target="_blank" rel="noopener noreferrer">
-            <FaFacebook />
+      <div className={styles.centerSection}>
+        <div className={styles.socialIcons}>
+          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faFacebookF} />
           </a>
-          <a href="https://twitter.com/YourHandle" target="_blank" rel="noopener noreferrer">
-            <FaTwitter />
+          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faTwitter} />
           </a>
-          <a href="https://instagram.com/YourProfile" target="_blank" rel="noopener noreferrer">
-            <FaInstagram />
+          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faInstagram} />
           </a>
-          <a href="https://linkedin.com/company/YourCompany" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin />
+          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedinIn} />
           </a>
         </div>
-        <p>All rights reserved for Imangi</p>
+      </div>
+      <div className={styles.footerLinks}>
+        <a href="/about" className={styles.link}>About Us</a>
+        <a href="/contact" className={styles.link}>Contact Us</a>
+        <a href="/faq" className={styles.link}>FAQ</a>
+        <a href="/feedback" className={styles.link}>Feedback</a>
+      </div>
+      <div className={styles.copyright}>
+        <p>
+          Copyright © 2024 Bigtree Entertainment Pvt. Ltd. All Rights Reserved.
+        </p>
       </div>
     </footer>
   );
