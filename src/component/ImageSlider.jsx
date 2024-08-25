fe4d95c694b3d4b8d25c1354ced22e1c5c73baf1
Anu Raj <aryanamit122@gmail.com>
2024-08-26 02:32:56 +0530

    newchanges

--- src/component/ImageSlider.jsx
+++ src/component/ImageSlider.jsx
@@ -16,7 +16,7 @@ const ImageSlider = () => {
     infinite: true,
     speed: 1000,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll:2,
     autoplay: true,
     autoplaySpeed: 3000,
     arrows: false,
