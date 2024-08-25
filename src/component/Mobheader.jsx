28edabc22dcb79d4da19579d615cc4e78a4e0eda
maheshwar04 <maheshwarnag04@gmail.com>
2024-08-26 03:40:42 +0530

    o

--- src/component/Mobheader.jsx
+++ src/component/Mobheader.jsx
@@ -32,8 +32,8 @@ const Mobheader = () => {
             <Link onClick={togglePopup}>
               <CgProfile className={style1.icon} />
               Profile
+              {showPopup && <Profile closePopup={togglePopup} />}
             </Link>
-            {showPopup && <Profile closePopup={togglePopup} />}
           </li>
         </div>
       </div>
