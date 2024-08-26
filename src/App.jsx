66878d034e014ccfc752b1bf5f273dc07f7418aa
Ashmit Raj <trivediashmit3@gmail.com>
2024-08-27 00:16:47 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import Header from "./component/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
 import Mobheader from "./component/Mobheader";
+import Herovideo from "./component/Herovideo";
 import ImageSlider from "./component/ImageSlider";
 import Footer from "./component/Footer";
 import Body from "./component/Body";
-import AdminDashboard from "./Dashboard/AdminDashboard";
 
 function App() {
-  const location = useLocation();
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  useEffect(() => {
-    const savedMode = localStorage.getItem("darkMode");
-    if (savedMode) {
-      setIsDarkMode(JSON.parse(savedMode));
-    }
-  }, []);
-
-  useEffect(() => {
-    document.body.className = isDarkMode ? "dark-mode" : "light-mode";
-    localStorage.setItem("darkMode", JSON.stringify(isDarkMode));
-  }, [isDarkMode]);
-
-  const toggleMode = () => {
-    setIsDarkMode(!isDarkMode);
-  };
+  const location = useLocation(); // Get the current location
 
   return (
     <>
-      {location.pathname !== "/admin" && <Header />}
-      <button onClick={toggleMode} className="mode-toggle">
-        {isDarkMode ? "Light Mode" : "Dark Mode"}
-      </button>
+      <Header />
       <Routes>
         <Route
           path="/"
@@ -46,9 +26,8 @@ function App() {
             </>
           }
         />
-        <Route path="/admin" element={<AdminDashboard />}></Route>
       </Routes>
-      {location.pathname !== "/admin" && <Footer />}
+      <Footer />
       <Mobheader />
     </>
   );
